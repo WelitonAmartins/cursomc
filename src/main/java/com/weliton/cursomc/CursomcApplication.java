3e6577a1f2c409d8d507cc0515ad9a5881d6ab3f
Weliton Martins <weliton.martinsprofissional@gmail.com>
2018-06-24 21:42:03 -0300

    Criando operacao de instaciacao

--- src/main/java/com/weliton/cursomc/CursomcApplication.java
+++ src/main/java/com/weliton/cursomc/CursomcApplication.java
@@ -1,12 +1,31 @@
 package com.weliton.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.weliton.cursomc.domain.Categoria;
+import com.weliton.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+	
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 }
